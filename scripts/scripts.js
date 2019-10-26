33880cca22b2c651735576d63a98b71543c5cfbe
PavKmiec <pavelkmiec07@gmail.com>
2019-10-26 14:11:34 +0100

    hostem_map

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
  // Initialize and add the map
  function initMap() {
-  // The location of Uluru
+  // The location of Hostel
   var hostel = {lat: 56.890030, lng: -4.7935728};
-  // The map, centered at Uluru
+  // The map, centered at Hostel
   var map = new google.maps.Map(
       document.getElementById('googleMap'), {zoom: 15, center: hostel});
-  // The marker, positioned at Uluru
+  // The marker, positioned at Hostel
   var marker = new google.maps.Marker({position: hostel, map: map});
 }
